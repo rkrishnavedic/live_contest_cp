104084bb7821218aa0a7931dca11c2c525533bcc
rkrishnavedic <57654450+rkrishnavedic@users.noreply.github.com>
2020-03-14 20:41:06 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+//#include "Header.h"
+
+#define pi 3.14159265358979323846264
+
+#define ll long long
+#define fori(n) for(long long i=0;i<n;i++)
+#define forj(n) for(long long j=0;j<n;j++)
+
+#define sp(n) setprecision(n)
+#define d7 1000000007
+#define mkp make_pair
+#define mkt make_tuple
+
+using namespace std;
+
+bool isPrime(long long k){
+    if(k<=1){return false;}
+    if(k==2){return true;}
+    if(k%2==0){return false;}
+    long long i = 3;
+   while (i*i <= k) {if (k % i == 0) {return false;}i += 2;}
+return true;
+}
+
+ll binomialCoeff(ll n, ll k){
+    ll C[n + 1][k + 1];ll i, j;
+    for (i = 0; i <= n; i++){
+        for (j = 0; j <= min(i, k); j++){
+            if (j == 0 || j == i){C[i][j] = 1;}
+            else{C[i][j] = (C[i - 1][j - 1]%d7 + C[i - 1][j]%d7)%d7;}}}
+    return C[n][k]%d7;
+ }
+
+ll gcd(ll a, ll b){if (b == 0)
+        return a;return gcd(b, a % b);}
+
+ll srihrg(ll n,ll k,ll m){
+    ll ans;
+    ans=1;
+    fori(k){
+        ans=(ans*n)%m;
+    }
+    return ans;
+}
+
+
+int main(int argc, const char * argv[]) {
+ //cout<<"Om\n";
+ //Praise to Lord
+ //make cin/cout faster
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);cout.tie(NULL);
+// code now
+   
+    ll n;
+    cin>>n;
+    vector<ll> v;
+    map<ll,ll> mp;
+    fori(n){
+        mp[v[i]]++;
+    }
+    cout<<mp.size();
+    
+//code ends
+    return 0;
+}
